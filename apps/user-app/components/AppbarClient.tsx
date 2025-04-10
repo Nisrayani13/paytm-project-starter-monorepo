b92ebbf6fee1df04b2ecc531c78c857d4fe572a3
Nisrayani13 <nisrayani.13@gmail.com>
2025-04-10 19:40:24 +0530

    step-16 done

---
+++ apps/user-app/components/AppbarClient.tsx
@@ -0,0 +1,24 @@
+"use client";
+
+import { Appbar } from "@repo/ui/appbar";
+import { signIn, signOut, useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
+
+export function AppbarClient() {
+  const session = useSession();
+  const router = useRouter();
+
+  return (
+    <>
+      <Appbar
+        user={session.data?.user}
+        onSignin={() => signIn()}
+        onSignout={async () => {
+          await signOut({
+            callbackUrl: "/api/auth/signin",
+          });
+        }}
+      ></Appbar>
+    </>
+  );
+}
